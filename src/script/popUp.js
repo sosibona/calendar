261cb3f8514cb4f9555dda7349ca4dd094404948
sosibona <empty.tara@gmail.com>
2020-02-26 13:39:20 +0200

    valid form

--- src/script/popUp.js
+++ src/script/popUp.js
@@ -30,12 +30,13 @@ const formElem = document.querySelector('.create-event');
 
 export function createEvent(event){
   event.preventDefault();
+
+  if(!isCorrect()) return;
+
   const formatData = [...new FormData(formElem)]
       .reduce((events, [field, value]) => ({...events, [field]: value}), {});
   formatData.data = new Date(formatData.data.replace(/-/g, ","));
-  console.log(formatData);
   events.push(formatData);
-  // console.log(copyEvents);
   console.log(events);
   makeEvent(events)
   closePopUp();
@@ -46,7 +47,47 @@ btnAddEvent.addEventListener('click', addEvent);
 btnClosePopUp.addEventListener('click', closePopUp);
 btnCreateEvent.addEventListener('click', createEvent);
 
- 1
+function isCorrect(){
+  const nameEvent = document.querySelector('.create-event__name');
+  const calendarEvent = document.querySelector('.calendar-data');
+  const startTimeEvent = document.querySelector('.startTime');
+  const endTimeEvent = document.querySelector('.endTime');
+  const error = document.querySelector('.modal-form__error');
+  error.innerHTML = '';
+
+  const year = new Date().getFullYear();
+  const month = new Date().getMonth();
+  const date = new Date().getDate();
+  // console.log(year,month,date);
+  const fullDate = new Date(year, month, date).getTime();
+  // `${year}-${month}-${date}`;
+
+  if (startTimeEvent.value === "" || endTimeEvent.value === "") {
+    error.innerHTML += 'wrong oclock';
+    console.log('wrong oclock');
+    return
+  }
+  const [StartHour, StartMinutes] = startTimeEvent.value.split(':').map(elem => +elem);
+  const [EndHour, EndMinutes] = endTimeEvent.value.split(':').map(elem => +elem);
+
+  console.log(StartHour, StartMinutes, EndHour, EndMinutes);
+  
+
+  console.log('fullDate');
+  
+
+  if (nameEvent.value === "") {
+    error.innerHTML += 'wrong name';
+    console.log('wrong name');
+    return false;
+  }
+  if (new Date(calendarEvent.value.replace(/-/g, ",")).getTime() < fullDate) {
+    error.innerHTML += 'wrong date';
+    return false;
+  }
+
+  return true;
+}
 
 
 
